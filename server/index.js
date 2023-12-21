5723fef02ac4b510febb16e47a07513b07a6957a
Nadja Spångberg <117263836+nasp22@users.noreply.github.com>
2023-12-22 00:42:30 +0100

    Added 'Access-Control-Allow-Methods' to allow PUT-request

--- server/index.js
+++ server/index.js
@@ -31,7 +31,8 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Headers', '*')
+    res.set('Access-Control-Allow-Headers', '*');
+    res.set('Access-Control-Allow-Methods', '*');
     next();
 });
 
