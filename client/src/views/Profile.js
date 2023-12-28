c5b32967e17516bf2666bc00de2dc807bba9fab8
Nadja Spångberg <nasp22@student.bth.se>
2023-12-29 00:51:03 +0100

    fix data, auth_id for validation

--- client/src/views/Profile.js
+++ client/src/views/Profile.js
@@ -3,9 +3,12 @@ import { Container, Row, Col } from "reactstrap";
 import Loading from "../components/Loading";
 
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import SignedInUser from "../components/SignedInUser";
 
 export const ProfileComponent = () => {
   const { user } = useAuth0();
+  const dbuser = SignedInUser()
+  console.log(dbuser.role)
 
   return (
     <Container className="mb-5">
@@ -20,6 +23,7 @@ export const ProfileComponent = () => {
         <Col md>
           <h2>{user.name}</h2>
           <p className="lead text-muted">{user.email}</p>
+          <p className="lead text-muted">Roll: {dbuser.role}</p>
         </Col>
       </Row>
       <Row>
