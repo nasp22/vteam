9244b628f3a8d002b8caca21b45642ee12c91910
schnell <christopher.schnell@hotmail.com>
2024-01-03 18:35:35 +0100

    authentication fixed and minor update for better navigation

--- client/src/components/Log.jsx
+++ client/src/components/Log.jsx
@@ -1,14 +1,28 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { useAuth0 } from "@auth0/auth0-react";
 import SignedInUser from "./SignedInUser";
 
 const Log = () => {
-  const user = SignedInUser();
-  console.log(user.log);
+  const { user } = useAuth0();
+  const loggedInUser = SignedInUser();
 
   return (
-    <div>
-      <h2>Logg</h2>
-      <p>{user.log}</p>
-    </div>
+    <Container className="mb-5">
+      <Row className="align-items-center profile-header mb-5 text-center text-md-left">
+        <Col md={2}>
+          <img
+            src={user.picture}
+            alt="Profile"
+            className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
+          />
+        </Col>
+        <Col md>
+          <h2>Logg</h2>
+          <p>{loggedInUser.log}</p>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
