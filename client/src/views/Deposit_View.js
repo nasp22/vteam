38ec4222a2f5512cd67772826e4ea19e775d6ae7
schnell <christopher.schnell@hotmail.com>
2024-01-03 18:32:12 +0100

    minor update

--- client/src/views/Deposit_View.js
+++ client/src/views/Deposit_View.js
@@ -2,15 +2,15 @@ import Deposit from '../components/Deposit';
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import Loading from '../components/Loading';
 
-
 const Deposit_View = () => {
-    return (
-      <div>
-        <Deposit/>
-      </div>
-    );
-  };
 
-  export default withAuthenticationRequired(Deposit_View, {
-    onRedirecting: () => <Loading />,
-  });
+  return (
+    <div>
+      <Deposit/>
+    </div>
+  );
+};
+
+export default withAuthenticationRequired(Deposit_View, {
+  onRedirecting: () => <Loading />,
+});
--- client/src/views/Log_View.js
+++ client/src/views/Log_View.js
@@ -1,19 +1,15 @@
 import Log from '../components/Log';
-import SignedInUser from '../components/SignedInUser';
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import Loading from '../components/Loading';
 
 const Log_View = () => {
-  let user = SignedInUser();
 
-    return (
-      <>
-      <div>
-        <Log/>
-      </div>
-      </>
-    );
-  };
+  return (
+    <div>
+      <Log/>
+    </div>
+  );
+};
 
 export default withAuthenticationRequired(Log_View, {
     onRedirecting: () => <Loading />,
