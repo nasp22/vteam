dae2a115bfbf3e40a10f035585ce3f8d911fa86e
Nadja Spångberg <117263836+nasp22@users.noreply.github.com>
2024-01-11 20:53:49 +0100

    Update Deposit_View.js

--- client/src/views/Deposit_View.js
+++ client/src/views/Deposit_View.js
@@ -2,18 +2,6 @@ import Deposit from '../components/Deposit';
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import Loading from '../components/Loading';
 
-const Deposit_View = () => {
-<<<<<<< HEAD
-=======
-
-    return (
-      <div>
-        <Deposit/>
-      </div>
-    );
-  };
->>>>>>> 89447c1d0900adf5462b4eec9c5b953bda5f9783
-
   return (
     <div>
       <Deposit/>
